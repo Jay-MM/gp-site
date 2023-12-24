0b99a13c896901140bb0437669a6e9a7f22f7885
Jesus M <marquez.jay444@gmail.com>
2023-12-24 01:48:47 -0600

    integrates email api, adds toasts and styles

--- dev/script.js
+++ dev/script.js
@@ -2,6 +2,13 @@
 const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
 const navbarToggler = document.querySelector('.navbar-toggler');
 const navbar = document.getElementById("myNavbar");
+const toast = new bootstrap.Toast(document.getElementById('liveToast'));
+
+function showToast(toastElement) {
+  let toast = new bootstrap.Toast(toastElement);
+  toast.show();
+}
+
 
 function closeNavbar() {
   // Close the navbar by triggering a click on the toggler button
@@ -29,12 +36,61 @@ function handleScroll() {
     navbar.style.backgroundColor = "transparent";
   }
 }
+
 function navbarFn() {
   addClickEventListeners();
   window.addEventListener("scroll", handleScroll);
 }
 
+(function () {
+  emailjs.init('tSLOkCyqkKzdDotLy');
+})();
 
+document.querySelector('#contact .btn-primary').addEventListener('click', function (event) {
+  event.preventDefault();
 
+  const contactForm = document.getElementById('contact-form');
+  const successToast = new bootstrap.Toast(document.getElementById('successToast'));
+  const errorToast = new bootstrap.Toast(document.getElementById('errorToast'));
+
+  var contactModalEl = document.getElementById('contactModal');
+  var contactModal = bootstrap.Modal.getInstance(contactModalEl);
+  if (!contactModal) {
+    contactModal = new bootstrap.Modal(contactModalEl);
+  }
+
+  function handleResponse(response, toast) {
+    console.log('SUCCESS!');
+
+    // Clear the input fields in the form
+    contactForm.reset();
+
+    // Show the appropriate toast
+    toast.show();
+
+    // Close the modal on successful submission
+    contactModal.hide();
+  }
+
+  emailjs.sendForm('default_service', 'contact_form', contactForm)
+    .then(function (response) {
+      if (response.status === 200) {
+        handleResponse(response, successToast);
+      } else {
+        handleResponse(response, errorToast);
+      }
+    })
+    .catch(function (error) {
+      console.log('FAILED...', error);
+
+      // Clear the input fields in the form
+      contactForm.reset();
+      // Show error toast
+      errorToast.show();
+
+      // Close the modal on error
+      contactModal.hide();
+    });
+});
 // Call the functions
 document.addEventListener("DOMContentLoaded", navbarFn);
